ed945af775f7f7d231b25947be64d2568dd108f4
ChrisLukova <oyeyochrislukova@gmail.com>
2026-01-31 12:56:16 +0300

    Shortened it

--- src/main/java/com/vaulta/vaulta_backend/config/SecurityConfig.java
+++ src/main/java/com/vaulta/vaulta_backend/config/SecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
@@ -36,7 +37,7 @@ public class SecurityConfig {
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(jwtFilter,
-                        org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter.class
+                        UsernamePasswordAuthenticationFilter.class
                         );
         return http.build();
     }
